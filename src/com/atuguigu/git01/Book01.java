c834ebd39e0da66eafc92b107eac6f5e59cf867c
khl775826762 <2659045808@qq.com>
2016-12-29 12:02:47 +0800

    第一次修改 KHL

--- src/com/atuguigu/git01/Book01.java
+++ src/com/atuguigu/git01/Book01.java
@@ -6,6 +6,9 @@ public class Book01 {
 		// TODO Auto-generated method stub
 	  //KHL 首次创建
      System.out.println("Git01---->book1:三生三世   xiaobao01");
+	  
+    //KHL 第一次修改
+     System.out.println("Git01---->book1:三生三世  -->:KHL第一次修改");
 	}
 
 }
